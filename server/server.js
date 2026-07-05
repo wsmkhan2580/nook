a161dca03c20ef61648e932b6e8a0f663776bcf6
wsmkhan2580 <choudharywaseem154@gmail.com>
2026-07-05 11:14:26 +0530

    Update API URL

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 // --- Middleware ---
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "https://nook-2hwd.onrender.com",
   })
 );
 app.use(express.json()); // parses JSON bodies — required or req.body is undefined
